d93e8127606a423bd4334fe10987c025194977e8
ranran <931094193@qq.com>
2017-06-06 18:46:22 +0800

    增加 mail，修改 junitt为44版本，将 spring tteest 的版本修改为统一的

---
+++ src/test/java/com/spiderdt/common/notice/service/EmailServiceTest.java
@@ -0,0 +1,13 @@
+import junit.framework.TestCase;
+
+/**
+ * @author ranran
+ * @version V1.0
+ * @Title:
+ * @Package com.spiderdt.common.notice.service
+ * @Description:
+ * @date 2017/6/6 15:17
+ */
+public class EmailServiceTest extends TestCase {
+
+}
