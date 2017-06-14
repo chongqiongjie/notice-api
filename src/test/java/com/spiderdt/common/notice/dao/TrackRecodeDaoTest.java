3510c49c324999808477d2697fc8296309b516de
ranran <931094193@qq.com>
2017-06-14 18:41:57 +0800

    单元测试修改bug

--- src/test/java/com/spiderdt/common/notice/dao/TrackRecodeDaoTest.java
+++ src/test/java/com/spiderdt/common/notice/dao/TrackRecodeDaoTest.java
@@ -1,5 +1,11 @@
 package com.spiderdt.common.notice.dao;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 /**
  * @author ranran
  * @version V1.0
@@ -8,5 +14,17 @@ package com.spiderdt.common.notice.dao;
  * @Description:
  * @date 2017/6/14 18:09
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/applicationContext.xml")
 public class TrackRecodeDaoTest {
+
+    @Autowired
+    private TrackRecodeDao trackRecodeDao;
+
+    @Test
+    public void testTrackRecodeDao() throws Exception {
+        boolean result = trackRecodeDao.updateTrackRecodeStatus("track_url_suffix_test", 5);
+        System.out.println(result);
+    }
+
 }
