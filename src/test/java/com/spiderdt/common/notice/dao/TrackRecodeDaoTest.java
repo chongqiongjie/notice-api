685b91d12ef7cca04455b3a8a39977a1b33763e8
ranran <931094193@qq.com>
2017-06-14 18:38:26 +0800

    自己的sql测试文件

---
+++ src/test/java/com/spiderdt/common/notice/dao/TrackRecodeDaoTest.java
@@ -0,0 +1,12 @@
+package com.spiderdt.common.notice.dao;
+
+/**
+ * @author ranran
+ * @version V1.0
+ * @Title:
+ * @Package com.spiderdt.common.notice.dao
+ * @Description:
+ * @date 2017/6/14 18:09
+ */
+public class TrackRecodeDaoTest {
+}
