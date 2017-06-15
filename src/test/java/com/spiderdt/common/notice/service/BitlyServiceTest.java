1db64ef2740b6c4976d0f22e20e9beed0d7aca6e
ranran <931094193@qq.com>
2017-06-15 17:31:23 +0800

    get short url test

--- src/test/java/com/spiderdt/common/notice/service/BitlyServiceTest.java
+++ src/test/java/com/spiderdt/common/notice/service/BitlyServiceTest.java
@@ -1,4 +1,10 @@
-import static org.junit.Assert.*;
+package com.spiderdt.common.notice.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * @author ranran
@@ -8,6 +14,27 @@ import static org.junit.Assert.*;
  * @Description:
  * @date 2017/6/15 16:22
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/applicationContext.xml")
 public class BitlyServiceTest {
+
+    @Autowired
+    BitlyService bitlyService;
+
+    @Test
+    public void getShortUrl() throws Exception {
+
+        String shortUrl = bitlyService.getShortUrl("http://www.baidu.com/track/9e34b9e1e4b3294de6f2cf4d5adbee5275f6d7f22b30e4b2cac5e659b8358e6c");
+        System.out.println(shortUrl);
+
+    }
+
+    @Test
+    public void getShortUrlBatch() throws Exception {
+    }
+
+    @Test
+    public void getResutlFromHttp() throws Exception {
+    }
 
 }
