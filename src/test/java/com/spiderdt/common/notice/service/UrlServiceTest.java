26f1fb3552f9469fd98ab9834a7fd8d8c95ccfde
ranran <931094193@qq.com>
2017-06-15 14:44:21 +0800

    sql modify

---
+++ src/test/java/com/spiderdt/common/notice/service/UrlServiceTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+/**
+ * @author ranran
+ * @version V1.0
+ * @Title:
+ * @Package com.spiderdt.common.notice.service
+ * @Description:
+ * @date 2017/6/15 9:59
+ */
+public class UrlServiceTest {
+
+}
